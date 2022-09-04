f34b3540c2e02bd61c362f33c41d008bdf13ee42
maymememaung <73875703+maymememaung@users.noreply.github.com>
2022-09-04 00:42:25 -0400

    updated create shipment

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
+from fileinput import filename
 import shippo
+from datetime import datetime
 
 shippo.config.api_key = "shippo_test_4be2330f4cc15d6d278b60de93c2adbc7ded0d1d"
 
@@ -13,21 +15,69 @@ possible_cmds = {
     -1: "Quit"
 }
 
+dist_units = {
+    0: "cm",
+    1: "in",
+    2: "ft",
+    3: "mm",
+    4: "m",
+    5: "yd"
+}
+
+mass_units = {
+    0: "g",
+    1: "oz",
+    2: "lb",
+    3: "kg"
+}
+
+
 def printCommands():
 
-    print ("These are the possible commands with codes.")
+    print("These are the possible commands with codes.")
 
     for code, cmd in possible_cmds.items():
         print("%s [%d]" % (cmd, code))
 
-def validateCommand(input):
+
+def printDistUnits():
+
+    print("These are the possible distance units for your parcel dimensions.")
+
+    for code, unit in dist_units.items():
+        print("%s [%d]" % (unit, code))
+
+
+def printMassUnits():
+
+    print("These are the possible mass units for your parcel weight.")
+
+    for code, unit in mass_units.items():
+        print("%s [%d]" % (unit, code))
+
+
+def validateCommand(input, dict):
 
     try:
         code = int(input)
     except:
         return False
 
-    return code in possible_cmds
+    return code in dict
+
+
+def validateDimensions(dimensions):
+
+    for d in dimensions:
+        try:
+            d = int(d)
+        except:
+            return False
+        finally:
+            if d < 0:
+                return False
+
+    return True
 
 
 def commandInput():
@@ -35,18 +85,145 @@ def commandInput():
     printCommands()
     code = input("Enter command code.\n")
 
-    while not (validateCommand(code)):
+    while not (validateCommand(code, possible_cmds)):
         printCommands()
         code = input("Enter command code.\n")
 
     return int(code)
 
-def addressInput():
-    pass
+
+def createAddress(logfile):
+
+    name = input("Enter Name:")
+    country = input("Enter the two letter country code, e.g. US:")
+    street = input("Enter the street address:")
+    city = input("Enter the city:")
+    state = input("Enter the state:")
+    zip = input("Enter the zip code:")
+
+    address_from = shippo.Address.create(
+        name=name,
+        street1=street,
+        city=city,
+        state=state,
+        zip=zip,
+        country=country,  # iso2 country code
+    )
+
+    print("Address Created. Address ID: %s" % (address_from.object_id))
+    logfile.write("Address Created. Address ID: %s\n" % (address_from.object_id))
+
+    add = {
+        "name": name,
+        "street1": street,
+        "city": city,
+        "state": state,
+        "zip": zip,
+        "country": country
+    }
+
+    return add
 
 
-def createShipment():
-    pass
+def createParcel(logfile):
+
+    printDistUnits()
+    dist_unit = input("\nEnter the distance unit code:")
+
+    while not (validateCommand(dist_unit, dist_units)):
+        print("Code not valid. Try again.")
+        printDistUnits()
+        dist_unit = input("Enter the distance unit code:")
+
+    dimensions = input(
+        "\nEnter length, width, and height separated by space:").split()
+    while (not validateDimensions(dimensions)):
+        print("\nDimension not valid. Try again.")
+        dimensions = input(
+            "\nEnter length, width, and height separated by space:").split()
+
+    printMassUnits()
+    mass_unit = input("\nEnter the mass unit code:")
+
+    while (not validateCommand(mass_unit, mass_units)):
+        print("Code not valid. Try again.")
+        printMassUnits()
+        mass_unit = input("\nEnter the mass unit code:")
+
+    weight = input("\nEnter your parcel weight.")
+    while (not validateDimensions([weight])):
+        print("\nWeight not valid. Try again.")
+        weight = input("\nEnter your parcel weight.")
+
+    parcel = shippo.Parcel.create(
+        length=dimensions[0],
+        width=dimensions[1],
+        height=dimensions[2],
+        distance_unit=dist_units[int(dist_unit)],
+        weight=weight,
+        mass_unit=mass_units[int(mass_unit)]
+    )
+
+    print("Parcel Created. Parcel ID: %s" % (parcel.object_id))
+    logfile.write("Parcel Created. Parcel ID: %s\n" % (parcel.object_id))
+
+    parcel = {
+        "length": dimensions[0],
+        "width": dimensions[1],
+        "height": dimensions[2],
+        "distance_unit": dist_units[int(dist_unit)],
+        "weight": weight,
+        "mass_unit": mass_units[int(mass_unit)]
+    }
+
+    return parcel
+
+
+def confirmShipment(sender_add, rcpt_add, parcel):
+
+    print("\nSender Name and Address: %s" % (" ".join(sender_add.values())))
+
+    print("\nRecipient Name and Address: %s" % (" ".join(rcpt_add.values())))
+
+    print("\nParcel Information: %s" % (" ".join(parcel.values())))
+    
+    confirm = input("\nDo you want to continue creating this shipment? [y|n]")
+
+    return confirm.lower() == "y"
+
+
+def createShipment(logfile):
+    print("\nYou are about to create a shipment.")
+
+    print("\nEnter the sender address.")
+
+    sender_add = createAddress(logfile)
+
+    print("\nEnter the recipient address.")
+
+    rcpt_add = createAddress(logfile)
+
+    print("\nEnter the parcel information.")
+
+    parcel = createParcel(logfile)
+
+    confirm = confirmShipment(sender_add, rcpt_add, parcel)
+
+    if (confirm):
+
+        shipment = shippo.Shipment.create(
+            address_from=sender_add,
+            address_to=rcpt_add,
+            parcels=[parcel],
+            asynchronous=False
+        )
+
+        print("Shipment Created. Shipment ID: %s" % (shipment.object_id))
+        logfile.write("Shipment Created. Shipment ID: %s\n" % (shipment.object_id))
+
+    else:
+        print("Aborting Operation.")
+
 
 if __name__ == "__main__":
 
@@ -54,17 +231,21 @@ if __name__ == "__main__":
 
     print("-----------------------------------------")
 
-    while not quit:
+    filename = datetime.now().strftime("%m-%d-%y,%H.%M.%S") + ".txt"
 
-        cmd_code = commandInput()
+    with open("logs/" + filename, "w") as logfile:
+        
+        while not quit:
 
-        match cmd_code:
-            case -1:
-                quit = True
-            case 0:
-                createShipment()
+            cmd_code = commandInput()
 
-        print("-----------------------------------------")
+            match cmd_code:
+                case -1:
+                    quit = True
+                case 0:
+                    createShipment(logfile)
+
+            print("-----------------------------------------")
 
 # address1 = shippo.Address.create(
 #     name='John Smith',
